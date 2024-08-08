d19ebeee8cd538b76c23eae2a88a87027f3cbc0c
Carrington <hartis.c99@gmail.com>
2024-08-08 18:47:32 -0400

    Gather player's already learned profession skills

--- GoTrain.lua
+++ GoTrain.lua
@@ -2,17 +2,32 @@
 SLASH_GOTRAIN1 = "/gotrain"
 
 local function GoTrainHandler()
+    -- All spells in object
     function printCraftableItemIds(items)
         for key, value in pairs(items) do
             for _, item in ipairs(value) do
                 print("Craftable Item ID: " .. item.id)
-                name, rank, icon, castTime, minRange, maxRange 
-                = GetSpellInfo(item.id)
+                name, rank, icon, castTime, minRange, maxRange = GetSpellInfo(item.id)
                 print("SPELL NAME: " .. name .. " DESCRIPTION: " .. GetSpellDescription(item.id))
             end
         end
     end
-    printCraftableItemIds(CraftableItems)
+    -- printCraftableItemIds(CraftableItems)
+
+    -- Gather player's already learned profession skills
+    local knownSkills = {}
+    local tradeskillName, currentLevel, maxLevel, skillLineModifier = GetTradeSkillLine()
+
+    for i = 1, GetNumTradeSkills() do
+        name, type, _, _, _, _ = GetTradeSkillInfo(i);
+        if (name and type ~= "header") then
+            table.insert(knownSkills, name)
+        end
+    end
+    print("KNOWN SKILLS: ")
+    for _, name in ipairs(knownSkills) do
+        print(name)
+    end
 end
 
 -- Tell WoW about the command
