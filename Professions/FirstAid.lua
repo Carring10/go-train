38f38407c9a14cbf5f9233ae4e6185db08c0e47f
Carrington <hartis.c99@gmail.com>
2024-08-10 12:51:52 -0400

    more first aid item data

--- Professions/FirstAid.lua
+++ Professions/FirstAid.lua
@@ -1,15 +1,31 @@
 function setFirstAidItems()
     CraftableItems = {
         [1] = {{
-            id = 3275
+            id = 3275 -- Linen Bandage
         }},
-        [40] = {{
-            id = 3276
+        [20] = {{
+            id = 3276 -- Heavy Linen Bandage
+        }},
+        [50] = {{
+            id = 3277 -- Wool Bandage
+        }},
+        [75] = {{
+            id = 3278 -- Heavy Wool Bandage
         }},
         [80] = {{
-            id = 7934
-        }, {
-            id = 3277
+            id = 7934 -- Anti-Venom
+        }},
+        [100] = {{
+            id = 7928 -- Silk Bandage
+        }},
+        [125] = {{
+            id = 7929 -- Heavy Silk Bandage
+        }},
+        [150] = {{
+            id = 10840 -- Mageweave Bandage
+        }},
+        [175] = {{
+            id = 10841 -- Heavy Mageweave Bandage
         }}
     }
 end
